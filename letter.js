f8835401bf6c13363f04814f784a8dadc3baef22
Handrew <andrew.a.han@gmail.com>
2018-04-20 20:54:30 -0400

    created skeleton for the letter.js

---
+++ letter.js
@@ -0,0 +1,11 @@
+function Letter(lettersToGuess, guessed) {
+    this.lettersToGuess = lettersToGuess;
+    this.guessed = guessed;
+    this.switchBlankWithLetter = function() {
+        
+    };
+    this.checking = function() {
+
+    };
+
+}
